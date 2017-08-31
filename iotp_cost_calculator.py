ad6030b480fb4a8d0db91925b76ebdf3ab0961c5
Kevin Eifinger <eifinger@de.ibm.com>
2017-08-31 12:14:02 +0200

    V2. - Wait time to 3hours between sends. - continue statement instead of loop_breaker. - iterate through sending times first as large sending times tend to be the problem - store more information in db

--- iotp_cost_calculator.py
+++ iotp_cost_calculator.py
@@ -1,4 +1,5 @@
 from cloudant import Cloudant
+from cloudant.error import CloudantDatabaseException
 import atexit
 import os
 import json
@@ -26,8 +27,8 @@ print "Start Date is {}".format(start_date)
 if 'VCAP_SERVICES' in os.environ:
     vcap = json.loads(os.getenv('VCAP_SERVICES'))
     print('Found VCAP_SERVICES')
-    print('This means cloud foundry. Setting wait_time to 7200')
-    wait_time = 7200
+    print('This means cloud foundry. Setting wait_time to 10800')
+    wait_time = 10800
 elif os.path.isfile('vcap-local.json'):
     with open('vcap-local.json') as f:
         vcap = json.load(f)
@@ -76,38 +77,34 @@ print("Starting iotp-cost-calculator")
 qos_levels = [0, 1, 2];
 sending_times = [10, 100, 1000, 10000, 100000, 1000000];
 
-loop_breaker = 0
-
 usage_params = {'start': start_date, 'end': time.strftime("%Y-%m-%d")}
 old_usage = apiClient.getDataTraffic(usage_params)
 print ("Old Usage Traffic: {}".format(old_usage))
 
 print("Input File contains {} JSON Objects".format(len(jsonconf['sizes'])))
-for jsoninput in jsonconf['sizes']:
-    print("JSON input size is: {}".format(jsoninput))
-    actual_size = len(json.dumps(jsonconf['sizes'][jsoninput]).encode('utf-8'))
-    print("Calculated size is: {}".format(actual_size))
-    for qos in qos_levels:
-        print("Messages with QoS: {}".format(qos))
-        for sending_time in sending_times:
-            print("Sending messages {} times.".format(sending_time))
-            for document in db:
-                time.sleep(0.3) # Wait so we dont exceed query/s limit
-                if document['sending_time'] == sending_time:
-                    if document['qos'] == qos:
-                        if document['actual_size'] == actual_size:
-                            print("Found document containing data for this combination: {}".format(document['_id']))
-                            loop_breaker = 1
-                            break
-            if loop_breaker == 1:
-                loop_breaker = 0
-                break
+for sending_time in sending_times:
+    print("Sending messages {} times.".format(sending_time))
+    for jsoninput in jsonconf['sizes']:
+        print("JSON input size is: {}".format(jsoninput))
+        actual_size = len(json.dumps(jsonconf['sizes'][jsoninput]).encode('utf-8'))
+        print("Calculated size is: {}".format(actual_size))
+        for qos in qos_levels:
+            print("Messages with QoS: {}".format(qos))
+            doc_id = str(wait_time) + "_" + str(qos) + "_" + str(sending_time) + "_" + str(actual_size)
+            try:
+                document = db[doc_id]
+                print("Found document containing data for this combination: {}".format(document['_id']))
+                time.sleep(1) #Wait 1 second so db calls are not faster than 20/s
+                continue
+            except KeyError:
+                print("Document does not yet exist. Continue...")
             t0 = time.time()
             times_interrupted = 0
+            print("Starting to send messages")
             for i in range(0,sending_time):
                 if (i+1)%100002 == 0:
-                    print("i is {}. Mod is 0. Waiting 10 seconds".format(i))
-                    time.sleep(10)
+                    print("i is {}. Mod is 0. Waiting 60 seconds".format(i))
+                    time.sleep(60)
                     times_interrupted+=1
                 appClient.publishEvent(device_type, device_id, "calculator-event", "json", jsonconf['sizes'][jsoninput], qos=qos)
             time_took = round(time.time()-t0, 3)-(10*times_interrupted)
@@ -118,12 +115,16 @@ for jsoninput in jsonconf['sizes']:
             usage_params = {'start': start_date, 'end': time.strftime("%Y-%m-%d")}
             new_usage = apiClient.getDataTraffic(usage_params)
             print("Data Usage Information: {}".format(new_usage))
-            doc_id = time.asctime( time.localtime(time.time()) ).replace(" ","-")
-            information = {"_id": doc_id,"old_data_usage": old_usage['total'],"reported_data_usage": new_usage['total'],"delta_data_usage": new_usage['total'] - old_usage['total'],"assumed_delta_data_usage": sending_time * actual_size,"sending_time": sending_time,"qos": qos,"actual_size": actual_size, "time_took": time_took}
+            storage_datetime = time.asctime( time.localtime(time.time()) ).replace(" ","-")
+            storage_timestamp = time.time()
+            information = {"_id": doc_id,"storage_datetime": storage_datetime,"storage_timestamp": storage_timestamp,"old_data_usage": old_usage['total'],"reported_data_usage": new_usage['total'],"delta_data_usage": new_usage['total'] - old_usage['total'],"assumed_delta_data_usage": sending_time * actual_size,"sending_time": sending_time,"qos": qos,"actual_size": actual_size, "time_took": time_took}
             print("Storing information under doc_id: {}".format(doc_id))
-            new_doc = db.create_document(information)
-            if new_doc.exists():
-                print("Successfully stored information")
+            try:
+                new_doc = db.create_document(information,throw_on_exists=True)
+                if new_doc.exists():
+                    print("Successfully stored information")
+            except CloudantDatabaseException:
+                print("Document was created from another application in the meantime. Skip...")
             old_usage = new_usage
             print("information: {}".format(information))
 
